8f1727a77ff991e85242b10cd437096f58557985
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-05-07 12:58:00 +0300

    about-slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -7,7 +7,12 @@ const swiper = new Swiper('.swiper', {
   pagination: {
     clickable: true,
     el: '.swiper-pagination',
+    dynamicBullets: true,
   },
+  autoplay: {
+    delay: 2000,
+  },
+  speed: 900,
   slidesPerView: 1.3,
   spaceBetween: 15,
   // Navigation arrows
@@ -15,9 +20,27 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
 
   // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
 });
